cf99372c8f5d5ecfc7c471a62e25711266367b35
shalant <doug.rosenberg@gmail.com>
2023-09-11 21:37:50 -0500

    making a lot of progress with CRUD, wiring, and authorization

--- BloggieToBike/BloggieToBike/Pages/Admin/Create.cshtml.cs
+++ BloggieToBike/BloggieToBike/Pages/Admin/Create.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using BloggieToBike.Models;
 using BloggieToBike.Web.Data;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BloggieToBike.Pages.NewBikeRoutes
 {
